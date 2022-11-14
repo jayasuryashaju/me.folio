fb10dd59729261fea51828ecd50c5fecb298a652
jayasuryashaju <82130065+jayasuryashaju@users.noreply.github.com>
2022-11-14 18:52:39 +0530

    Add files via upload

---
+++ assets/js/js2.js
@@ -0,0 +1,108 @@
+/*form validation*/
+
+
+
+
+function namecheck()
+{
+    let name=document.getElementById("name").value
+    let nm=null
+     nm = name.trim()
+//    var letters = /^[A-Za-z]+$/;
+//    var spc=/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+[A-Za-z]*$/;
+    document.getElementById("name").value = nm;
+    if(nm==""||name==null)
+    {
+        document.getElementById("nmlabel").style.display="block"
+        return false
+    }
+    else if(!isNaN(nm[0]))
+    {
+        document.getElementById("nmlabel").style.display="none"
+        document.getElementById("nmlabel-01").style.display="block"
+        return false
+    }
+    // else if(nm.match(spc))
+    // {
+    //     document.getElementById("nmlabel").style.display="none" 
+    //     document.getElementById("nmlabel-01").style.display="none"  
+    //     document.getElementById("nmlabel-02").style.display="block"
+    //     return false
+    // }
+    else{
+        document.getElementById("nmlabel").style.display="none" 
+        document.getElementById("nmlabel-01").style.display="none" 
+        document.getElementById("nmlabel-02").style.display="none"
+        return true
+
+    }
+}
+
+
+function emailcheck()
+{
+    var val=document.getElementById("email").value
+    var v=null
+     v = val.trim()
+    document.getElementById("email").value = v;
+    if(v==""||val==null)
+    {
+        document.getElementById("el").style.display="block"
+        return false
+    }
+    
+    else if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v))
+  {
+    document.getElementById("el-02").style.display="block"
+    return false
+  }
+
+    else{
+        document.getElementById("el").style.display="none" 
+        document.getElementById("el-02").style.display="none" 
+        return true
+
+    }
+}
+
+function msgcheck()
+    {
+    
+        var val=document.getElementById("msg").value
+        var v=null
+         v = val.trim()
+        document.getElementById("msg").value = v;
+        var n=v.length
+        console.log(n)
+        if(v==""||val==null)
+        {
+            document.getElementById("ml").style.display="block"
+            return false
+        }
+        else{
+            document.getElementById("ml").style.display="none"   
+            return true
+    
+        }
+    }
+  
+
+function subjectcheck()
+{
+    let name=document.getElementById("subject").value
+    let nm=null
+     nm = name.trim()
+
+    document.getElementById("subject").value = nm;
+    if(nm==""||name==null)
+    {
+        document.getElementById("sub").style.display="block"
+        return false
+    }
+    else{
+        document.getElementById("sub").style.display="none" 
+        
+        return true
+
+    }
+}
