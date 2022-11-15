c8d5ed0ae87e14ff93c04f280ffe0fe7ed9a5b0a
jayasuryashaju <82130065+jayasuryashaju@users.noreply.github.com>
2022-11-15 16:57:44 +0530

    Update js2.js

--- assets/js/js2.js
+++ assets/js/js2.js
@@ -8,8 +8,6 @@ function namecheck()
     let name=document.getElementById("name").value
     let nm=null
      nm = name.trim()
-//    var letters = /^[A-Za-z]+$/;
-//    var spc=/^[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+[A-Za-z]*$/;
     document.getElementById("name").value = nm;
     if(nm==""||name==null)
     {
@@ -22,13 +20,6 @@ function namecheck()
         document.getElementById("nmlabel-01").style.display="block"
         return false
     }
-    // else if(nm.match(spc))
-    // {
-    //     document.getElementById("nmlabel").style.display="none" 
-    //     document.getElementById("nmlabel-01").style.display="none"  
-    //     document.getElementById("nmlabel-02").style.display="block"
-    //     return false
-    // }
     else{
         document.getElementById("nmlabel").style.display="none" 
         document.getElementById("nmlabel-01").style.display="none" 
